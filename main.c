9df6408cc4068679df88cf9924522ca2826eed53
Nicholas Jones <nj23629@gmail.com>
2019-04-10 07:36:50 +0000

    introduction to print f and scan f statements

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-rotationcipher ()
 int main () {
-int a=0,b=1,c=2,d=3,e=4,f=5,g=6,h=7,i=8,j=9,k=10,l=11,m=12,n=13,o=14,p=15,q=16,r=17,s=18,t=19,u=20,v=21,w=22,x=23,y=24,z=25;// inputting letters of alphabet
-int e(x);// encryption 
-scanf("Enter a message to encrypt using rotational cipher:");
+
+int e;// encryption 
+int n;
+int g;
+int phrase[24];
+printf("Enter a phrase\n");
+scanf ("%d", &g);
+printf("Please choose a rotational cipher option:\n");
+printf("To encrypt enter: E \n");
+printf("To decrypt enter: D \n");
+scanf("%d", &n);
+
+
+
 
 
 

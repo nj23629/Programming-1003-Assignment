0ba8e6026927c9588cdfeba8e50e21d16a6c968f
Nicholas Jones <nj23629@gmail.com>
2019-04-10 08:28:41 +0000

    updates

--- main.c
+++ main.c
@@ -1,30 +1,43 @@
 #include <stdio.h> // standard line to begin code
-int main () { // 
+int main () 
+{ // 
 
-int e;// intitialising integer e
-int n; //initialising integer n 
+char e;
+int n; 
+char E;
+char D;
 
-char phrase[100];// intialising function phrase
-printf("Enter a phrase\n");// printf statement prompts user to enter a phrase
-scanf("%d", &n);// This line is used to read text a line at a time
-printf("Please choose a rotational cipher option:\n");// printf statement prints the line to screen telling user to choose an option
-printf("To encrypt enter: E \n");// printf statement prints line to screen telling first option
-printf("To decrypt enter: D \n");// printf statements prints line to screen telling second option
-scanf("%d", &n);// scanf statement allows user to input one of the above options E or D
-
-
-    printf("You have chosen to encrpyt the phrase\n");
-    printf("Enter rotational encryption amount\n");
+char phrase[2];
+printf("Enter a phrase\n");
+scanf("%s", phrase);
 
 
+printf("Please choose a rotational cipher option:\n");
+printf("To encrypt enter: E \n");
+printf("To decrypt enter: D\n");
 
+scanf(" %c", &e);
 
+for(e==E)
+{ printf("encryption selected\n");
 
+}
 
+for(e==D)
+{ printf("decryption selected\n");
 
+}
+
+   
 
 return 0;
 }
+
+
+
+
+
+
 
 
 // from document e(x)= m + k (%26)- due to 26 letters in alphabet e meaning encryption

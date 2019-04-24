0efcf663d0c0d86141417c3e627c581242b83162
Nicholas Jones <nj23629@gmail.com>
2019-04-24 06:59:40 +0000

    WOOHOO ROTATION CIPHER DONEEE

--- main.c
+++ main.c
@@ -1,66 +1,84 @@
 #include <stdio.h> // standard line to begin code
-int main () 
-{ // 
+int main () // main area of code everything initialised here is present inside loops and statements
+{ 
 
-int n; 
+int n;// initialises n the number used to select option
 int k;// k is the number being used for rotational cipher
 
 
-char phrase[80];
-printf("Enter a phrase - using CAPS\n");
-gets(phrase);
+char phrase[80], MSG;// intialises phrase that can also be noted as MSG(Message)
+printf("Enter a phrase - using CAPS\n");// prints to screen "Enter a phrase- using caps" this instructs user
+gets(phrase);// statement used to allow whitespace in text inputted above
 
 
-printf("Please choose rotational or substitution cipher option:\n");
+printf("Please choose rotational or substitution cipher option:\n");// The following lines print the options to screen for user to follow
 printf("To encrypt using rotation cipher enter: 1 \n");
 printf("To decrypt using rotation cipher enter: 2\n");
 printf("To encrypt using substitution cipher enter: 3\n");
 printf("To decrypt using substitution cipher enter: 4\n");
 
-scanf(" %d", &n);
+scanf(" %d", &n);// allows user to input a number from above for selection
 
-if(n==1){
-    printf("Rotational encryption selected of (%s):\n", phrase);
-    printf("Enter an encryption number:\n");
-    scanf(" %d", &k);
-    char encryption[80], phrase;
-    int i;
-    for (i=0; encryption[i] != '\0'; ++i)
+if(n==1){// this if statement is initialised if the number 1 is inputted above
+    printf("Rotational encryption selected of (%s):\n", phrase); // prints to screen telling user rotational encryption has been selected
+    printf("Enter an encryption number:\n");// prints to screen instructing user to select a number
+    scanf(" %d", &k);// scanf allows user to input rotational number
+    
+    int j;
+    for (j=0; phrase[j] != '\0'; ++j)
     {
-        phrase= encryption[i];
-    if (phrase >= 'A' && phrase <= 'Z') 
+        MSG = phrase[j];
+    
+    if (MSG >= 'A' && MSG <= 'Z') 
     {
-        phrase= encryption + k;
-    if (phrase > 'Z') 
+        MSG = MSG + k;
+    if (MSG > 'Z') 
     {
-        phrase= encryption - 'Z' + 'A'-1;
+        MSG = MSG - 'Z' + 'A'-1;
     }
-      encryption[i]= phrase;
+      phrase[j]= MSG;
     }
     }
-    printf("The encryption is: %s\n", encryption);
+    printf("The encryption is: %s\n", phrase);
     
     
 }
 
-if(n==2){
+if(n==2){ //this if statement is initialised if the number 2 is inputted above
     printf("Rotational decryption selected of (%s):\n", phrase);
     printf("Enter an decryption number:\n");
     scanf(" %d", &k);
+    
+    int j;
+    for (j=0; phrase[j] != '\0'; ++j)
+    {
+        MSG = phrase[j];
+    
+    if (MSG >= 'A' && MSG <= 'Z') 
+    {
+        MSG = MSG - k;
+    if (MSG > 'Z') 
+    {
+        MSG = MSG + 'Z' - 'A'-1;
+    }
+      phrase[j]= MSG;
+    }
+    }
+    printf("The decryption is: %s\n", phrase);
 }
 
 
-if(n==3){ 
+if(n==3){ //this if statement is initialised if the number 3 is inputted above
     printf("Subsitution encryption selected of (%s):\n", phrase);
 }
 
 
-if(n==4){
+if(n==4){ //this if statement is initialised if the number 4 is inputted abov
     printf("Substitution decryption selected of (%s):\n", phrase);  
 }
 
-if(n<1, n>4){
-    printf("Error!! Not an available option >:(");
+if(n<1, n>4){ //this if statement is initialised if the numbers above are not selected
+    printf("Error!! Not an available option >:(");// prints statement to screen demonstrating to user that error
 }
 
 

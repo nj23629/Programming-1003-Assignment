90c731196034f3a8b8ece672d8e3062b3f589174
Nicholas Jones <nj23629@gmail.com>
2019-04-27 07:36:57 +0000

    substitution cipher encryption sorted-- only decryption to go

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>		// standard line to begin code
-int
-main ()				// main area of code everything initialised here is present inside loops and statements
+
+int main ()				// main area of code everything initialised here is present inside loops and statements
 {
   
- 
+char substitutionkey[26];// initialises substitutionkey used in substitution cipher
 int n;			// initialises n the number used to select option
 int k;			// k is the number being used for rotational cipher
 int j;          // initialises j which is used in rotationl cipher
+
   
  
 char phrase[80], MSG;	// intialises phrase that can also be noted as MSG(Message)
@@ -39,7 +40,7 @@ if (n == 1)
 MSG = phrase[j];
 	  
  
-if (MSG >= 'A' && MSG <= 'Z')	// demonstrates if the letters are bew
+if (MSG >= 'A' && MSG <= 'Z')	//
 	    {
 	      
 MSG = MSG + k;	// uses the encryption number k to select how many letter spots have moved
@@ -103,16 +104,99 @@ printf ("The decryption is: %s\n", phrase);	// prints to screen with the decrypt
  
 if (n == 3)
     {				//this if statement is initialised if the number 3 is inputted above
-      printf ("Subsitution encryption selected of (%s):\n", phrase);
-    
+      printf ("Substitution encryption selected of (%s):\n", phrase);
+      printf("Enter a substitution alphabet key:\n");
+      scanf("%s", substitutionkey);
+      int f= strlen (substitutionkey);
+      
+      if (f %26 !=0)
+      {
+          printf("Make sure to have a 26 key alphabet not using same letter twice");
+          return 0;
+      }
+      
+      int p;
+      int q= strlen (phrase);
+      char *ephrase = (char *) malloc (sizeof (char) * q);
+
+
+for (int p = 0; p < q; p++)
+	{
+
+int b = (phrase[p]) - 'A';
+
+if (b >= 0 && b < 26)
+	    {
+
+
+ephrase[p] = substitutionkey[b];
+
 }
+
+	  else
+	    {
+
+ephrase[p] = phrase[p];
+
+}
+
+}
+
+
+
+printf ("The encryption is: %s\n", ephrase);
+
+      
+}
+
+
+
   
  
  
 if (n == 4)
     {				//this if statement is initialised if the number 4 is inputted abov
       printf ("Substitution decryption selected of (%s):\n", phrase);
-    
+      printf("Enter a substitution alphabet key:\n");
+      scanf("%s", substitutionkey);
+      int f= strlen (substitutionkey);
+      
+      if (f %26 !=0)
+      {
+          printf("Make sure to have a 26 key alphabet not using same letter twice");
+          return 0;
+      }
+      
+      int p;
+      int q= strlen (phrase);
+      char *ephrase = (char *) malloc (sizeof (char) * q);
+
+
+for (int p = 0; p < q; p++)
+	{
+
+int b = (phrase[p]) - 'A';
+
+if (b >= 0 && b < 26)
+	    {
+ 
+
+
+
+}
+
+	  else
+	    {
+
+ephrase[p] = phrase[p];
+
+}
+
+}
+
+
+
+printf ("The encryption is: %s\n", ephrase);
 }
   
  
